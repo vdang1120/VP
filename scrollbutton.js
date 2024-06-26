5a987df56d70ca0b09ac5c4ab419d11a60660817
vdang1120 <vdang1120@gmail.com>
2024-06-26 12:11:02 -0500

    slideshow time

---
+++ scrollbutton.js
@@ -0,0 +1,18 @@
+let scrollButton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "flex";
+    scrollButton.style.justifyContent = "center";
+    scrollButton.style.alignItems = "center";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
