8c6e35c701d3d8f6dfe63224e56e074cf4b827a0
vdang1120 <vdang1120@gmail.com>
2024-06-26 12:18:31 -0500

    slideshow time again

--- scrollbutton.js
+++ scrollbutton.js
@@ -3,7 +3,7 @@ let scrollButton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     scrollButton.style.display = "flex";
     scrollButton.style.justifyContent = "center";
     scrollButton.style.alignItems = "center";
